58d1acf3bfa9b306281b94ebe31f89518b4d035e
Briana King <brianaking1330@gmail.com>
2022-10-16 15:16:59 -0600

    Adding return values...

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,45 +5,45 @@ public class Main {
   public static void main(String[] args) {
     Calculator calc = new Calculator();
     boolean on = true;
-    System.out.print("Welcome to the Calculator!");
+    System.out.print("Welcome to the Calculator!\n");
 
 
     Scanner sc=new Scanner(System.in);  //System.in is a standard input stream
     while(on) {
-      System.out.print("Enter a Command: ");
+      System.out.print("Enter a Command: \n");
 
       String str=sc.next();
       //add
       switch (str) {
         case "add":
-          calc.add(sc.nextInt(), sc.nextInt());
+          System.out.println(calc.add(sc.nextInt(), sc.nextInt()));
           break;
         //subtract
         case "subtract":
-          calc.subtract(sc.nextInt(), sc.nextInt());
+          System.out.println(calc.subtract(sc.nextInt(), sc.nextInt()));
           break;
         //multiply
         case "multiply":
-          calc.multiply(sc.nextInt(), sc.nextInt());
+          System.out.println(calc.multiply(sc.nextInt(), sc.nextInt()));
           break;
         //divide
         case "divide":
-          calc.divide(sc.nextInt(), sc.nextInt());
+          System.out.println(calc.divide(sc.nextInt(), sc.nextInt()));
           break;
         //fib
         case "fibonacci":
-          calc.fibonacciNumberFinder(sc.nextInt());
+          System.out.println(calc.fibonacciNumberFinder(sc.nextInt()));
           break;
         //binary
         case "binary":
-          calc.intToBinaryNumber(sc.nextInt());
+          System.out.println(calc.intToBinaryNumber(sc.nextInt()));
           break;
         //quit
         case "quit":
           on=false;
           break;
         default:
-          System.out.print("Commands are add, subtract, multiply, divide, fibonacci, binary, and quit");
+          System.out.println("Commands are add, subtract, multiply, divide, fibonacci, binary, and quit");
           break;
       }
     }
